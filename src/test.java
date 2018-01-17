c2614de3c1c681c3d9b2e4f50bcd447e744348d7
BN <BN@DESKTOP-C171QSA>
2018-01-17 16:32:43 +0100

    modif

--- src/test.java
+++ src/test.java
@@ -3,7 +3,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test class");
+		System.out.println("test bite");
 	}
 
 }
