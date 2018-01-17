e3b31acb668ca6c9647bdb8f8f86fbe16e81c2d4
Kayzer1713 <ghislain.vidal1@gmail.com>
2018-01-17 17:01:23 +0100

    back to normal

--- src/test.java
+++ src/test.java
@@ -3,8 +3,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test bite");
-		System.out.println("test Kevin");
+		System.out.println("test normal");
 	}
 
 }
