ac0515a96474ca5b748735331e7e71aa3db2477f
BN <BN@DESKTOP-C171QSA>
2018-01-17 16:36:57 +0100

    test

--- src/test.java
+++ src/test.java
@@ -3,7 +3,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test bite");
+		System.out.println("test coucou");
 	}
 
 }
