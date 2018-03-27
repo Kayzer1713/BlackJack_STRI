8d33257f2eb9a2e170277c246f3f3dfc18564207
Kevin <kevin.nguyen6901@gmail.com>
2018-03-28 00:09:30 +0200

    update mise classe table

--- src/Model/Table.java
+++ src/Model/Table.java
@@ -16,21 +16,47 @@ public class Table {
 	 */
 	private Deck paquet;
 	
-	public Table(Joueur dealer) {
+	/**
+	 * Mise minimal requise pour jouer sur la table
+	 */
+	private int miseTable;
+	
+	/**
+	 * Boléen indiquant si table dynamique = 0, table permanente/statique = 1
+	 */
+	private boolean tablePermanente;
+	
+	/**
+	 * Nombre de joueurs sur la table
+	 */
+	private int nombreJoueurs;
+	
+	/**
+	 * Constructeur table
+	 * @param dealer
+	 */
+	public Table(Joueur dealer, int miseTable, boolean tablePermanente, int nombreJoueurs) {
 		ajoutJoueur(dealer.getNom(), dealer);
 		this.paquet = new Deck();
 		this.paquet.reset();
+		this.miseTable = miseTable;
+		this.tablePermanente = tablePermanente;
+		
 	}
 
 	/**
-	 * Ajoute un joueur à la table.
+	 * Ajoute un joueur à la table si la table n'est pas pleine.
 	 * @param s Socket du joueur
 	 * @param j joueur à ajouter
 	 * @see Joueur
 	 * @see Socket
 	 */
 	public void ajoutJoueur(String pseudo, Joueur j) {
-		this.listeJoueursTable.put(pseudo, j);
+		if(listeJoueursTable.size()-1 <= nombreJoueurs){
+			this.listeJoueursTable.put(pseudo, j);
+		} else {
+			System.out.println("Nombre de joueurs max atteint.");
+		}
 	}
 
 	/**
