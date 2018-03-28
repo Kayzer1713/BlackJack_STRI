20d1ad0a628ed5739f48f8a6094c425f023f2994
Kevin <kevin.nguyen6901@gmail.com>
2018-03-28 13:26:23 +0200

    update classe joueur & table

--- src/Model/Joueur.java
+++ src/Model/Joueur.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
 public class Joueur {
 
 	/**
+	 * 	
+	 */
+	private boolean dealer;
+
+	/**
 	 * Liste des cartes en main du joueur
 	 */
 	private ArrayList<Carte> main;
@@ -19,20 +24,26 @@ public class Joueur {
 	 * pseudo/nom du joueur
 	 */
 	private String nom;
-	
+
 	/**
 	 * Nombre de jetons du joueurs, 100 par défaut.
 	 */
 	private int jetons;
+	
+	private Table t; 
+	
+	private ObjectOutputStream out;
+
+	public Joueur(String pseudo) {
+		this.nom = pseudo;
+		this.dealer = true;
+	}
 
 	/**
 	 * Constructeur défault
 	 * @param nom du joueur
 	 */
-
-	private ObjectOutputStream out;
-
-	public Joueur(String pseudo, ObjectOutputStream out2) {
+	public Joueur(String pseudo, ObjectOutputStream out) {
 		this.nom = pseudo;
 		this.setOutJoueur(out);
 		this.jetons = 100;
@@ -51,6 +62,14 @@ public class Joueur {
 	public void setNom(String nom) 
 	{
 		this.nom = nom;
+	}
+
+	public Table getT() {
+		return t;
+	}
+
+	public void setT(Table t) {
+		this.t = t;
 	}
 
 	public ObjectOutputStream getOutJoueur() {
@@ -115,19 +134,23 @@ public class Joueur {
 	public void afficheMain() {
 		System.out.println(main);
 	}
-	
+
 	/**
 	 * Gain des jetons du joueur
 	 */
 	public void ajoutJetons(int mise){
 		this.jetons = this.jetons + mise;
 	}
-	
+
 	/**
 	 * Perte des jetons du joueur
 	 */
 	public void retireJetons(int mise){
 		this.jetons = this.jetons - mise;
+	}
+
+	public boolean isDealer() {
+		return this.dealer;
 	}
 
 }
--- src/Model/Table.java
+++ src/Model/Table.java
@@ -2,46 +2,51 @@ package Model;
 
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
-public class Table {
+import Controller.Serveur;
+
+public class Table extends Thread{
 
 	/**
 	 * Map contenant la liste des joueurs de la table
 	 */
 	private HashMap<String, Joueur> listeJoueursTable;
-	
+
 	/**
 	 * Paquet utilisé sur la table en question
 	 * @see Deck
 	 */
 	private Deck paquet;
-	
+
 	/**
 	 * Mise minimal requise pour jouer sur la table
 	 */
 	private int miseTable;
-	
+
 	/**
 	 * Boléen indiquant si table dynamique = 0, table permanente/statique = 1
 	 */
 	private boolean tablePermanente;
-	
+
 	/**
 	 * Nombre de joueurs sur la table
 	 */
-	private int nombreJoueurs;
-	
+	private int nbJoueurMax;
+
 	/**
 	 * Constructeur table
 	 * @param dealer
 	 */
-	public Table(Joueur dealer, int miseTable, boolean tablePermanente, int nombreJoueurs) {
+	public Table(Casino casino, Joueur dealer, int miseTable, boolean tablePermanente, int nbJoueurMax) {
+		this.listeJoueursTable = new HashMap<String, Joueur>();
 		ajoutJoueur(dealer.getNom(), dealer);
 		this.paquet = new Deck();
 		this.paquet.reset();
 		this.miseTable = miseTable;
 		this.tablePermanente = tablePermanente;
-		
+		this.nbJoueurMax = nbJoueurMax;
 	}
 
 	/**
@@ -52,7 +57,7 @@ public class Table {
 	 * @see Socket
 	 */
 	public void ajoutJoueur(String pseudo, Joueur j) {
-		if(listeJoueursTable.size()-1 <= nombreJoueurs){
+		if(listeJoueursTable.size()-1 <= nbJoueurMax){
 			this.listeJoueursTable.put(pseudo, j);
 		} else {
 			System.out.println("Nombre de joueurs max atteint.");
@@ -66,9 +71,55 @@ public class Table {
 	public void retireJoueur(String pseudo) {
 		this.listeJoueursTable.remove(pseudo);
 	}
-	
+
+	public void distribuer(Joueur j) {
+		j.ajoutCarte(this.paquet.prendreCarte());
+	}
+
+	/**
+	 * Donne une carte au joueur en mettant à jour le status de la table chez tout les clients
+	 */
+	public void majCarte() {
+		// met a jour les informations chez tout les clients qu'il y a eu un changement
+	}
 	public int nbJoueur() {
 		return this.listeJoueursTable.size()-1;
 	}
-	
+
+	@Override
+	public String toString() {
+		String description = "";
+		if ( this.tablePermanente )
+			description += "Permanante: ";
+		else
+			description += "Temporaire | ";
+		description += "Mise minimum:" + miseTable + " | Nb Joueurs: " + (listeJoueursTable.size()-1) + "/" + nbJoueurMax;
+		return description;
+	}
+
+	public void run() {
+		// parcour de la liste des joueurs:
+		Joueur jCourant;
+		
+		Set<String> cles = listeJoueursTable.keySet();
+		Iterator<String> it = cles.iterator();
+		
+		// distribution premier tour
+		while (it.hasNext()){
+			String cle = it.next();
+			jCourant = listeJoueursTable.get(cle);
+			distribuer(jCourant);
+		}
+		
+		// distribution deuxième tour
+		while (it.hasNext()){
+			String cle = it.next();
+			jCourant = listeJoueursTable.get(cle);
+			if (jCourant.isDealer() == true)
+				distribuer(jCourant);
+			distribuer(jCourant);
+			
+		}
+		
+	}
 }
